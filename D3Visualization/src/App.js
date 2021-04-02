d4d46fadd8ab1acfcdf04c504cf6e056ae087198
may811204 <ju40268@gmail.com>
2021-04-01 23:55:40 -0500

    fix the issue for loading data before the load is ready

--- D3Visualization/src/App.js
+++ D3Visualization/src/App.js
@@ -2,52 +2,49 @@ import { useState, useEffect, Suspense } from 'react';
 import ReactTooltip from 'react-tooltip';
 import MapChart from './components/MapChart';
 import './App.css';
-import LocationCard from "./components/LocationCard";
 
 function App() {
-  const [content, setContent] = useState(null);
-  const [state, setUSState] = useState(null);
-  const [vender, setVender] = useState(null);
+    const [content, setContent] = useState(null);
+    const [state, setUSState] = useState(null);
+    const [vender, setVender] = useState(null);
 
-  useEffect(() => {
-    fetch(`https://www.vaccinespotter.org/api/v0/states/${state}.json`)
-      .then(response => response.json())
-      .then(data => setVender({data}));
-  }, [state]);
+    console.log(vender)
 
-  return (
-      <div>
-          <div className="container">
-              <Suspense fallback={<div>Fetching results...</div>}>
-                  <Suspense fallback={<>Loading...</>}>
-                      <h2 className="title">
-                          Covid 19 Vaccine Providers
-                      </h2>
-                  </Suspense>
-                  <MapChart setTooltipContent={setContent} setUSState={setUSState} />
-              </Suspense>
-              <ReactTooltip
-                  className="tooltip"
-                  textColor="black"
-                  backgroundColor="white"
-              >
-                  {content && (
-                      <>
-                          <h3>{content.name}</h3>
-                          <p>{content.provider_count} available providers count</p>
-                          <p>{content.total_provider_count} available stores count</p>
-                          <p>Available detailed information</p>
-                          {content.provider_brands.map((brand, i) => {
-                              return <li key={brand.id}><b>{brand.name}</b> store count: {brand.location_count} - <a href={brand.url}>Visit</a></li>;
-                          })}
-                      </>
-                  )}
-              </ReactTooltip>
-          </div>
-          {vender && <LocationCard vender={vender} />}
-      </div>
-
-  );
+    useEffect(() => {
+        fetch(`https://www.vaccinespotter.org/api/v0/states/${state}.json`)
+            .then(response => response.json())
+            .then(data => setVender({data}));
+    }, [state]);
+    return (
+        <div className="container">
+            <Suspense fallback={<div>Fetching results...</div>}>
+                <Suspense fallback={<>Loading...</>}>
+                    <h1>Covid 19 Vaccine Tracker</h1>
+                </Suspense>
+                <MapChart setTooltipContent={setContent} setUSState={setUSState} />
+            </Suspense>
+            <ReactTooltip
+                className="tooltip"
+                textColor="black"
+                backgroundColor="white"
+            >
+                {content && (
+                    <>
+                        <h3>{content.name}</h3>
+                        <p className="elect-total">{content.provider_count} available providers count</p>
+                        <p className="elect-total">{content.total_provider_count} available stores count</p>
+                        <p className="elect-total">Available detailed information</p>
+                        {content.provider_brands.map((brand, i) => {
+                            return <li key={brand.id}><b>{brand.name}</b> store count: {brand.location_count}</li>;
+                        })}
+                    </>
+                )}
+            </ReactTooltip>
+            {vender && vender?.data.features.slice(0, Math.min(vender?.data.features.length, 10)).map((store, i) => {
+                return <div className='card'><b>{store.properties.name}</b>: {store.properties.address}, {store.properties.city} {store.properties.state}</div>
+            })}
+        </div>
+    );
 }
 
 export default App;
--- D3Visualization/src/components/MapChart.js
+++ D3Visualization/src/components/MapChart.js
@@ -2,142 +2,134 @@ import { useEffect, useState, memo } from 'react';
 import { geoCentroid } from 'd3-geo';
 import { scaleQuantize } from "d3-scale";
 import {
-  ComposableMap,
-  Geographies,
-  Geography,
-  Marker,
-  Annotation,
+    ComposableMap,
+    Geographies,
+    Geography,
+    Marker,
+    Annotation,
 } from 'react-simple-maps';
 import ReactTooltip from 'react-tooltip';
 import allStates from '../data/allstates.json';
-import * as constants from '../constants';
 
 const geoUrl = 'https://cdn.jsdelivr.net/npm/us-atlas@3/states-10m.json';
 
 const offsets = {
-  VT: [50, -8],
-  NH: [34, 2],
-  MA: [30, -1],
-  RI: [28, 2],
-  CT: [35, 10],
-  NJ: [34, 1],
-  DE: [33, 0],
-  MD: [47, 10],
-  DC: [49, 21],
+    VT: [50, -8],
+    NH: [34, 2],
+    MA: [30, -1],
+    RI: [28, 2],
+    CT: [35, 10],
+    NJ: [34, 1],
+    DE: [33, 0],
+    MD: [47, 10],
+    DC: [49, 21],
 };
+
 const colorScale = scaleQuantize()
-    .domain([100, 1000])
+    .domain([0, 1000])
     .range([
-        "#ffedea",
-        "#ffcec5",
-        "#ffad9f",
-        "#ff8a75",
-        "#ff5533",
-        "#e2492d",
-        "#be3d26",
-        "#9a311f",
-        "#782618"
+        "#f9fbfd",
+        "#dce7f3",
+        "#b0cae5",
+        "#92b6db",
+        "#6699cc",
+        "#578fc7",
+        "#3e7cb9",
+        "#2f5e8d",
+        "#1b3752"
     ]);
 
 const MapChart = ({ setTooltipContent, setUSState }) => {
-  const [loading, setLoading] = useState(true);
-  const [vaccineData, setVaccineData] = useState(null);
-  useEffect(() => {
-    fetch('https://www.vaccinespotter.org/api/v0/states.json')
-      .then(response => response.json())
-      .then(data => setVaccineData({data}))
-      .finally(() => setLoading(false))
-  }, [])
+    const [vaccineData, setVaccineData] = useState(null);
+    useEffect(() => {
+        fetch('https://www.vaccinespotter.org/api/v0/states.json')
+            .then(response => response.json())
+            .then(data => setVaccineData({data}));
+    }, [])
 
-  useEffect(() => {
-    ReactTooltip.rebuild();
-  }, []);
+    useEffect(() => {
+        ReactTooltip.rebuild();
+    }, []);
 
-  const handleHover = (geoId, name) => {
-    const cur = allStates.find((s) => s.val === geoId);
-    const stateData = vaccineData.data.find((e) => e.code === cur.id)
-    setTooltipContent({
-      name: stateData.name,
-      provider_count: stateData.provider_brand_count,
-      total_provider_count: stateData.store_count,
-      provider_brands: stateData.provider_brands
-    });
-    ReactTooltip.rebuild();
-  };
+    const handleMouseEnter = (geoId, name) => {
+        const cur = allStates.find((s) => s.val === geoId);
+        const stateData = vaccineData.data.find((e) => e.code === cur.id)
+        setTooltipContent({
+            name: stateData.name,
+            provider_count: stateData.provider_brand_count,
+            total_provider_count: stateData.store_count,
+            provider_brands: stateData.provider_brands
+        });
+        ReactTooltip.rebuild();
+    };
+
     const handleClick = (geoId) => {
         const cur = allStates.find((s) => s.val === geoId);
-        setUSState(cur.id) // TX, CA
+        setUSState(cur.id)
     }
-    const calculateColorScale = (geoId, name) => {
-      const cur = allStates.find((s) => s.val === geoId);
-      const stateData = vaccineData.data.find((e) => e.code === cur.id)
-      return colorScale(stateData?.store_count || 0)
-  }
 
-  if (loading) {
-      return <p>loading...</p>
-  } else {
-      return (
-      <ComposableMap data-tip="" projection="geoAlbersUsa">
-          <Geographies geography={geoUrl}>
-              {({ geographies }) => (
-                  <>
-                      {geographies.map((geo) => (
-                          <Geography
-                              key={geo.rsmKey}
-                              stroke="#FFF"
-                              geography={geo}
-                              style={{
-                                  default: {
-                                      fill: calculateColorScale(geo.id, geo.properties.name)
-                                  },
-                                  hover: {
-                                      fill: constants.RED
-                                  }
-                              }}
-                              onMouseEnter={() => handleHover(geo.id, geo.properties.name)}
-                              onClick={() => handleClick(geo.id)}
-                              onMouseLeave={() => setTooltipContent(null)}
-                          />
-                      ))}
-                      {geographies.map((geo) => {
-                          const centroid = geoCentroid(geo);
-                          const cur = allStates.find((s) => s.val === geo.id);
-                          return (
-                              <g key={geo.rsmKey + '-name'}>
-                                  {cur &&
-                                  centroid[0] > -160 &&
-                                  centroid[0] < -67 &&
-                                  (Object.keys(offsets).indexOf(cur.id) === -1 ? (
-                                      <Marker coordinates={centroid}>
-                                          <text
-                                              y="2"
-                                              fontSize={14}
-                                              textAnchor="left"
-                                              fill="#FFF"
-                                          >
-                                              {cur.id}
-                                          </text>
-                                      </Marker>
-                                  ) : (
-                                      <Annotation
-                                          subject={centroid}
-                                          dx={offsets[cur.id][0]}
-                                          dy={offsets[cur.id][1]}
-                                      >
-                                          <text>
-                                            {cur.id}
-                                          </text>
-                                      </Annotation>
-                                  ))}
-                              </g>
-                          );
-                      })}
-                  </>
-              )}
-          </Geographies>
-      </ComposableMap>
+    return (
+        <ComposableMap data-tip="" projection="geoAlbersUsa">
+            <Geographies geography={geoUrl}>
+                {({ geographies }) => (
+                    <>
+                        {geographies.map((geo) => {
+                            const centroid = geoCentroid(geo);
+                            const cur = allStates.find((s) => s.val === geo.id);
+                            const stateData = vaccineData?.data.find((e) => e.code === cur.id)
+                            return (
+                                <>
+                                    <Geography
+                                        key={geo.rsmKey}
+                                        stroke="#FFF"
+                                        geography={geo}
+                                        fill="#DDD"
+                                        style={{
+                                            default: {
+                                                fill: colorScale(stateData?.store_count || 0),
+                                                outline: 'none',
+                                            },
+                                            hover: {
+                                                fill: "#C53030",
+                                                outline: 'none',
+                                            }
+                                        }}
+                                        onMouseEnter={() => handleMouseEnter(geo.id, geo.properties.name)}
+                                        onClick={() => handleClick(geo.id)}
+                                        onMouseLeave={() => setTooltipContent(null)}
+                                    />
+                                    <g key={geo.rsmKey + '-name'}>
+                                        {cur &&
+                                        centroid[0] > -168 &&
+                                        centroid[0] < -67 &&
+                                        (Object.keys(offsets).indexOf(cur.id) === -1 ? (
+                                            <Marker coordinates={centroid}>
+                                                <text
+                                                    textAnchor="left"
+                                                    fill="#FFF"
+                                                >
+                                                    {cur.id}
+                                                </text>
+                                            </Marker>
+                                        ) : (
+                                            <Annotation
+                                                subject={centroid}
+                                                dx={offsets[cur.id][0]}
+                                                dy={offsets[cur.id][1]}
+                                            >
+                                                <text x={4} fontSize={14}>
+                                                    {cur.id}
+                                                </text>
+                                            </Annotation>
+                                        ))}
+                                    </g>
+                                </>
+                            );
+                        })}
+                    </>
+                )}
+            </Geographies>
+        </ComposableMap>
     );
-  }
 };
 export default memo(MapChart);
