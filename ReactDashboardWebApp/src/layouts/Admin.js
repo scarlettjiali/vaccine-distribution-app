d28b4327b422a7cecdfa2af14b6e381eb5288278
Jiali Sun <jialisun2018@gmail.com>
2021-04-25 14:38:20 -0700

    Fix image issue

--- ReactDashboardWebApp/src/layouts/Admin.js
+++ ReactDashboardWebApp/src/layouts/Admin.js
@@ -45,11 +45,13 @@ export default function Admin({ ...rest }) {
   // ref to help us initialize PerfectScrollbar on windows devices
   const mainPanel = React.createRef();
   // states and functions
-  // const [image, setImage] = React.useState(bgImage);
-  // const [color, setColor] = React.useState("blue");
+  // eslint-disable-next-line no-unused-vars
+  const [image, setImage] = React.useState(bgImage);
+  // eslint-disable-next-line no-unused-vars
+  const [color, setColor] = React.useState("blue");
 
-  const image = React.useState(bgImage);
-  const color = React.useState("blue");
+  // const image = React.useState(bgImage);
+  // const color = React.useState("blue");
   // const [fixedClasses, setFixedClasses] = React.useState("dropdown show");
   const [mobileOpen, setMobileOpen] = React.useState(false);
   // const handleImageClick = image => {
