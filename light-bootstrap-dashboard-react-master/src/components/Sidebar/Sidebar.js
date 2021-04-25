32b863d5e7aa8f8131c71fabad8b07e06c4a4021
Jiali Sun <jialisun2018@gmail.com>
2021-04-25 16:53:00 -0700

    Polish Style

--- light-bootstrap-dashboard-react-master/src/components/Sidebar/Sidebar.js
+++ light-bootstrap-dashboard-react-master/src/components/Sidebar/Sidebar.js
@@ -38,7 +38,6 @@ function Sidebar({ color, image, routes }) {
       <div className="sidebar-wrapper">
         <div className="logo d-flex align-items-center justify-content-start">
           <a
-            href="https://www.creative-tim.com?ref=lbd-sidebar"
             className="simple-text logo-mini mx-1"
           >
             <div className="logo-img">
@@ -48,8 +47,8 @@ function Sidebar({ color, image, routes }) {
               />
             </div>
           </a>
-          <a className="simple-text" href="http://www.creative-tim.com">
-            Creative Tim
+          <a className="simple-text">
+            TEAM 124
           </a>
         </div>
         <Nav>
